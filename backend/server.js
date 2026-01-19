c9c05cc544cc2f52a2780a9a77a32418ce8db150
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2026-01-19 13:16:59 +0100

    Add backend, frontend, public and related files

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
+// dependencies
+// ------------------------------------------------------
+
+
+// en minimal express server applikation
 import express from 'express';
+
+// miljövariabler / inställningar
+// ------------------------------------------------------
+const app = express();
+
+// enge en mapp som express kan använda för att skicka filer automatiskt utan routes
+app.use(express.static('frontend/public'))
+
+// definera/ange våran port
+const port = 8555;
+
+// middleware
+// ------------------------------------------------------
+
+
+// routes
+// ------------------------------------------------------
+
+
+// lyssna på event
+// ------------------------------------------------------
+
+
+// starta servern
+// ------------------------------------------------------
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
