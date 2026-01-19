17a97fe764028a1f083179226e9db2e862319a63
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2026-01-19 15:49:26 +0100

    Add eventListeners and ws,  chat now works both ways in console

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@
 import express from 'express';
 // http är en inbyggd dependencie, det behöver alltså inte installeras innan vi importerar 
 import http from 'http';
-import ws from 'ws';
+import {WebSocketServer} from 'ws';
 
 // miljövariabler / inställningar
 // ------------------------------------------------------
@@ -18,6 +18,31 @@ app.use(express.static('frontend/public'))
 // definera/ange våran port
 const port = 8555;
 
+// skapa en http server, express skickas med som en instans
+const server = http.createServer(app);
+
+// skapa en websocket server
+const wss = new WebSocketServer({noServer: true});
+
+// handskaningen - godkänn kommunikation via websocket
+server.on("upgrade", (req, socket, head) => {
+    
+    console.log("event upgrade...")
+
+    // bestäm vem som får kommunicera med websocket
+    // ex, kolla om man är inloggad
+    // if (!authenticated) return
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        
+        console.log("Client:", req.headers['user-agent']);
+
+        // kommunikation on, skicka vidare event med 'emit'
+        // använd händelselyssnare senare i koden
+        wss.emit("connection", ws, req);
+    });
+});
+
 // middleware
 // ------------------------------------------------------
 
@@ -28,10 +53,42 @@ const port = 8555;
 
 // lyssna på event
 // ------------------------------------------------------
+wss.on('connection', (ws) => {
+    
+    // info om klienter som autentiseras - websocket kommunikation ok
+    console.log(`Klient ansluten, antal klienter: ${wss.clients.size}`);
+
+    // skicka meddelande till browser-land
+    // skicka och ta emot data, förusätt att det är i JSON-format
+
+    const obj = {msg: "Ny klient ansluten :)"};
+
+    ws.send(JSON.stringify(obj));
+
+    // lyssna på event när en klient lämnar kommunikationen
+    ws.on('close', () => {
+
+        console.log(`Klient lämnade, antal klienter kvar: ${wss.clients.size}`);
+    });
+
+    // lyssna på event av sorten "message"
+    ws.on('message', (data) => {
+
+        const obj = JSON.parse(data);
+
+        console.log(obj);
+
+        wss.clients.forEach(client => {
+            client.send(JSON.stringify(obj))
+        })
+
+    })
+
+});
 
 
 // starta servern
 // ------------------------------------------------------
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
--- frontend/public/scripts/code.js
+++ frontend/public/scripts/code.js
@@ -20,6 +20,13 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     console.log("och nu då...");
+
+    // skicka ett meddelande via websocket
+    const msg = msgElement.value;
+
+    const obj = {msg: msg};
+
+    websocket.send(JSON.stringify(obj));
 })
 
 // aktivera lyssnare på input#msg: kan användas för att visa att ngn skriver tex "...is typing"
@@ -29,6 +36,18 @@ msgElement.addEventListener("keydown", (e) => {
     // ...hanter att en person skriver ngt - kan kanske skickas som en händelse backend...
 });
 
+// aktivera lyssnare på socket events
+websocket.addEventListener("message", (e) => {
+
+    const data = e.data;
+
+    // skicka och ta emot data, förusätt att det är i JSON-format
+
+    const obj = JSON.parse(e.data);
+    console.log("obj", obj);
+
+
+});
 
 
 // funktioner
