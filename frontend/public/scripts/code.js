c231c069723016837e54ccd3417dd59dd4601f02
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2026-02-04 10:45:27 +0100

    Edit user_left to update correctly with color and username

--- frontend/public/scripts/code.js
+++ frontend/public/scripts/code.js
@@ -16,20 +16,20 @@ const onlineUsersElement = document.getElementById("online-users");
 const canvas = document.querySelector("canvas");
 const logoutBtn = document.getElementById("logout-btn");
 
-// dependencies - WebSocket
-// const websocket = new WebSocket("ws://localhost:8555");
+// dependencies - WebSocket - Lokalt:
+// const host = window.location.hostname;
+// const websocket = new WebSocket(`ws://${host}:8555`);
+// const endpoint = `http://${host}:8555/login`;
+
+// dependencies - deploy public:
 const backendHost = "scribblesocial-case-9.onrender.com";
 const websocket = new WebSocket(`wss://${backendHost}`);
 const endpoint = `https://${backendHost}/login`;
 
+
 import Player from "./Player.js";
 
 let player;
-
-
-// console.log(player);
-
-
 
 // variabler, inställningar
 // ------------------------------------------------------
@@ -42,16 +42,6 @@ let points = [];
 const logoutButton = () => {
     location.reload();
 }
-// const currentUsers = [];
-
-
-// testa rita i canvas elementet
-// ------------------------------------------------------
-// beskriv en cirkelbåge
-// ctx.arc(canvas.width / 2, canvas.height / 2, 10, 0, Math.PI * 2, true);
-
-// rita
-// ctx.fill();
 
 
 // händelselyssnare
@@ -61,8 +51,6 @@ formUsername.addEventListener("submit", (e) => {
     e.preventDefault();
 
     username = userElement.value;
-
-    // const endpoint = "http://localhost:8555/login";
 
     const options = {
         method: "POST",
@@ -98,7 +86,6 @@ formUsername.addEventListener("submit", (e) => {
                 divMainBox.classList.remove("div-main-box");
                 divMainBox.classList.add("div-main-box-logged-in");
 
-                // resizeCanvas();
 
                 onlineUsersElement.textContent = data.username;
                 // rita 2d i canvas elementet
@@ -112,16 +99,8 @@ formUsername.addEventListener("submit", (e) => {
                 const obj = { type: "new_user", username: username, player: player };
                 websocket.send(JSON.stringify(obj));
 
-                // const userData = {
-                //     username: data.username,
-                //     id: data.id
-                // };
-
-                // sessionStorage.setItem("chatUser", JSON.stringify(userData));
-
             } else {
                 console.log("Username already in use");
-                // ge meddelande till klient: autentisering ej ok
             }
         })
 });
@@ -148,9 +127,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 formMessage.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    // endast autentiserade ska kunna chatta
-
 
     // skicka ett meddelande via websocket
     const msg = msgElement.value;
@@ -194,7 +170,6 @@ websocket.addEventListener("message", (e) => {
             break;
 
         case "new_client":
-            // console.log("uppdatera ngt på klientsidan", obj.usersOnline)
             break;
 
         case "new_user":
@@ -209,7 +184,12 @@ websocket.addEventListener("message", (e) => {
             break;
 
         case "user_left":
-            onlineUsersElement.textContent = obj.usersOnline;
+            onlineUsersElement.innerHTML = obj.usersOnline.map(user => `
+                <li>
+                    ${user.username}
+                    <span class="online-dot" style="background-color:${user.color};"></span>
+                </li>
+            `).join("");
             break;
 
         case "draw":
@@ -224,9 +204,6 @@ websocket.addEventListener("message", (e) => {
 });
 
 
-// window.addEventListener("load", resizeCanvas);
-// window.addEventListener("resize", resizeCanvas);
-
 // lägg till händelselyssnare för att kunna rita i ett canvas-element
 canvas.addEventListener("mousedown", (e) => {
     const rect = canvas.getBoundingClientRect();
@@ -237,14 +214,10 @@ canvas.addEventListener("mousedown", (e) => {
 
     if (!player) { return }
 
-    // const point = { x: e.offsetX, y: e.offsetY };
-    // console.log("mousedown");
     isDrawing = true;
 
     player.drawStart(canvas, ctx, point);
 
-    // ctx.beginPath();
-    // ctx.moveTo(point.x, point.y);
     points = [];
     // buffra koordinat (point)
     points.push(point);
@@ -261,22 +234,16 @@ canvas.addEventListener("touchmove", (e) => {
 
     if (!player) { return }
 
-    // const point = { x: e.offsetX, y: e.offsetY };
-    // console.log("mousedown");
     isDrawing = true;
 
     player.drawStart(canvas, ctx, point);
 
-    // ctx.beginPath();
-    // ctx.moveTo(point.x, point.y);
     points = [];
     // buffra koordinat (point)
     points.push(point);
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    // const point = { x: e.offsetX, y: e.offsetY };
-    // console.log(point);
     if (!isDrawing) { return };
     if (!player) { return };
 
@@ -287,16 +254,13 @@ canvas.addEventListener("mousemove", (e) => {
     };
 
     player.draw(canvas, ctx, point);
-    // ctx.lineTo(point.x, point.y);
-    // ctx.stroke();
 
     // buffra kordinater (points) och skicka klumpvis med ws
     points.push(point);
 });
 
 canvas.addEventListener("mouseup", (e) => {
-    // const points = { x: e.offsetX, y: e.offsetY };
-    // console.log("mouseup");
+
     if (!player) { return }
 
     isDrawing = false;
@@ -308,8 +272,6 @@ canvas.addEventListener("mouseup", (e) => {
         color: player.color,  // Ser till att obj.color inte blir undefined på servern
         username: player.username // Valfritt: bra för att veta vem som ritat
     };
-    // player.drawEnd(canvas, ctx);
-    // ctx.closePath();
 
     // sänd buffrade koordinater via websocket
     websocket.send(JSON.stringify(drawingData));
@@ -337,8 +299,6 @@ function renderChatMessage(obj) {
     const p = document.createElement("p");
     div.classList = "text-msg";
 
-    // applicera klass på vem som skriver - jfr username === obj.username
-
     if (obj.username !== username) {
         div.classList = "text-msg";
     } else {
@@ -359,7 +319,7 @@ function renderChatMessage(obj) {
     // aktuell tid
     const time = document.createElement("time");
 
-    // vad har obj.date för datatyo
+    // vad har obj.date för datatyp
     console.log("Datatyp:", typeof obj.date);
 
     // datumobjekt för att kunna välja ut en viss del
@@ -370,20 +330,10 @@ function renderChatMessage(obj) {
     time.dateTime = date.toLocaleTimeString();
     div.appendChild(time);
 
-
     chatElement.prepend(div);
-    // chatElement.appendChild(p);
     chatElement.scrollTop = 0;
 }
 
-// function resizeCanvas() {
-//     canvas.width = canvas.clientWidth;
-//     canvas.height = canvas.clientHeight;
-
-//     ctx = canvas.getContext("2d");
-// }
-
-// resizeCanvas();
 
 function drawLine(obj) {
 
@@ -410,8 +360,6 @@ function drawLine(obj) {
     ctx.closePath();
 
     ctx.fillText(obj.username, points[0].x, points[0].y);
-    // ctx.font = "14px";
-    // ctx_wrap.fillStyle(obj.color);
 }
 
 function refreshPage() {
